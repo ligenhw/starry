39af246c3ad1903221804935b14bb6ae419d2d0b
ligen60 <ligen60@jd.com>
2021-12-11 11:13:29 +0800

    CollectEvtEtlToEsJob enable checkpointting

--- src/main/scala/cn/bestlang/starry/evt/CollectEvtEtlToEsJob.scala
+++ src/main/scala/cn/bestlang/starry/evt/CollectEvtEtlToEsJob.scala
@@ -1,14 +1,16 @@
 package cn.bestlang.starry.evt
 
 import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.EnvironmentSettings
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 
 object CollectEvtEtlToEsJob {
 
   def main(args: Array[String]): Unit = {
-    val settings = EnvironmentSettings.newInstance.build
-    val tEnv = TableEnvironment.create(settings)
     val params = ParameterTool.fromArgs(args)
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.getConfig.setGlobalJobParameters(params)
 
     val topic = params.get("kafka.topic")
     val bootStrapServers = params.get("kafka.bootstrap.servers")
@@ -17,6 +19,12 @@ object CollectEvtEtlToEsJob {
     val esIndex = params.get("es.index")
     val esUsername = params.get("es.username")
     val esPassword = params.get("es.password")
+
+    val checkpointingEnabled = params.has("checkpointing")
+    if (checkpointingEnabled) env.enableCheckpointing(10000)
+
+    val settings = EnvironmentSettings.newInstance.build
+    val tEnv = StreamTableEnvironment.create(env, settings)
 
     tEnv.executeSql(
       """
